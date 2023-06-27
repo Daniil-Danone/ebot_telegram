b6f9028dedaa2a12d8da69e42b2add99d7cc497e
Danone <mr.super.ko@inbox.ru>
2023-06-27 13:16:26 +0600

    cringe

--- main.py
+++ main.py
@@ -16,25 +16,18 @@ links = []
 
 count = 0
 media_id = 0
+caption = ''
 
 
-@bot.message_handler(content_types=['text'])
-def welcome(message):
+@bot.message_handler(content_types=['text', 'photo'])
+def listener(message):
     con = sqlite3.connect('users.db')
     cur = con.cursor()
     cur.execute("""CREATE TABLE IF NOT EXISTS users(user_id INT PRIMARY KEY, name TEXT, email TEXT, flag TEXT);""")
     con.commit()
     cur.close()
 
-    if user_exists(message.from_user.id):
-        text = bot.send_message(message.chat.id,
-                                "Привет! Я EBot и готов к работе! Присылай сообщения и я перенаправлю их на твою почту")
-        bot.register_next_step_handler(text, listener)
-
     if message.text == '/start':
-        text = 'Привет, меня зовут EBot.'
-        bot.send_message(message.from_user.id, text)
-
         if user_exists(message.from_user.id) is None:
             user_id = message.from_user.id
 
@@ -42,7 +35,7 @@ def welcome(message):
             cur = con.cursor()
 
             insert = '''INSERT INTO users (user_id, flag) VALUES(?, ?);'''
-            data = (user_id, 'name',)
+            data = (user_id, 'name', )
 
             cur.execute(insert, data)
             con.commit()
@@ -51,75 +44,79 @@ def welcome(message):
             text = bot.send_message(message.chat.id, "Как Вас зовут?")
             bot.register_next_step_handler(text, registration_name)
 
-
-@bot.message_handler(content_types=['text', 'photo'])
-def listener(message):
-    if message.content_type == 'text':
-        if message.forward_from or message.forward_from_chat:
-
-            chat, sender = '', ''
-
-            if message.forward_from:
-                chat, sender = forward_from(message)
-
-            #  канал
-            elif message.forward_from_chat:
-                chat, sender = forward_from_chat(message)
-
-            converted_time = lambda x: time.strftime("%d.%M.%Y %H:%M:%S", time.localtime(x))
-
-            text = f'''<b>ВАЖНОЕ - Вы отметили данное сообщение</b>
-            <p><b>Чат:</b> <i>{chat}</i></p>
-            <p><b>Отправитель:</b> <i>{sender}</i></p>
-            <p><b>Время написания:</b> <i>{converted_time(message.forward_date)}</i></p>
-            <b>Оригинальное сообщение:</b>
-            <br>
-            <i>{message.text}</i>
-            '''
-
-            email = get_user_email(message.from_user.id)[0]
-
-            mail_send(text, email)
-
-            text = bot.reply_to(message, "Письмо отправлено на почту ✅")
-            bot.register_next_step_handler(text, listener)
-
         else:
-            text = bot.reply_to(message, "Жду следующего сообщения...")
-            bot.register_next_step_handler(text, listener)
-
-    elif message.content_type == 'photo':
-        global count, media_id
-
-        if media_id == message.media_group_id:
-            count += 1
-
-        if message.media_group_id:
-            media_id = message.media_group_id
-
-        file = bot.get_file(message.photo[-1].file_id)
-        filename, file_exstension = os.path.splitext(file.file_path)
-
-        downloaded_file = bot.download_file(file.file_path)
-        src = 'files/' + str(message.photo[-1].file_id) + file_exstension
-
-        with open(src, 'wb') as f:
-            f.write(downloaded_file)
-
-        link = upload(src, str(message.photo[-1].file_id) + file_exstension)
-
-        if link:
-            links.append(link)
-            text = bot.reply_to(message, "Этот файл скачан на Google Drive\n\n"
-                                         f"Ссылка на файл: {link}")
-            bot.register_next_step_handler(text, listener)
-
-        if len(links) == count:
-            print('ok-')
+            bot.send_message(message.chat.id,
+                             "Привет! Я EBot и готов к работе! Присылай сообщения и я перенаправлю их на твою почту")
 
     else:
-        text = bot.send_message(message.chat.id, f"Тип файла: {message.content_type}")
-        bot.register_next_step_handler(text, listener)
+        if message.content_type == 'text':
+            if message.forward_from or message.forward_from_chat:
+
+                chat, sender = '', ''
+
+                if message.forward_from:
+                    chat, sender = forward_from(message)
+
+                #  канал
+                elif message.forward_from_chat:
+                    chat, sender = forward_from_chat(message)
+
+                converted_time = lambda x: time.strftime("%d.%M.%Y %H:%M:%S", time.localtime(x))
+
+                text = f'''<b>ВАЖНОЕ - Вы отметили данное сообщение</b>
+                <p><b>Чат:</b> <i>{chat}</i></p>
+                <p><b>Отправитель:</b> <i>{sender}</i></p>
+                <p><b>Время написания:</b> <i>{converted_time(message.forward_date)}</i></p>
+                <b>Оригинальное сообщение:</b>
+                <br>
+                <i>{message.text}</i>
+                '''
+
+                email = get_user_email(message.from_user.id)[0]
+
+                mail_send(text, email)
+
+                bot.send_message(message.from_user.id, "Письмо отправлено на почту ✅")
+
+            else:
+                bot.send_message(message.from_user.id, "Жду следующего сообщения...")
+
+        elif message.content_type == 'photo':
+            global count, media_id, caption
+
+            if media_id == message.media_group_id:
+                count += 1
+
+            if message.media_group_id:
+                media_id = message.media_group_id
+
+            if message.caption:
+                caption = message.caption
+
+            file = bot.get_file(message.photo[-1].file_id)
+            filename, file_exstension = os.path.splitext(file.file_path)
+
+            downloaded_file = bot.download_file(file.file_path)
+            src = 'files/' + str(message.photo[-1].file_id) + file_exstension
+
+            with open(src, 'wb') as f:
+                f.write(downloaded_file)
+
+            link = upload(src, str(message.photo[-1].file_id) + file_exstension)
+
+            if link:
+                os.remove(src)
+                links.append(link)
+                bot.send_message(message.from_user.id, "Этот файл скачан на Google Drive\n\n"
+                                                       f"Ссылка на файл: {link}")
+
+            if len(links) == count:
+                media_id = 0
+                count = 0
+                send_email(message, caption)
+
+        else:
+            bot.send_message(message.from_user.id, f"Тип файла: {message.content_type}")
 
 
 def user_exists(user_id):
@@ -190,12 +187,12 @@ def forward_from_chat(message):
     return chat, sender
 
 
-def send_email(message):
+def send_email(message, caption):
     global links
 
-    try:
-        caption = message.caption
+    time.sleep(20)
 
+    try:
         chat, sender = '', ''
 
         if message.forward_from:
@@ -207,6 +204,12 @@ def send_email(message):
 
         converted_time = lambda x: time.strftime("%d.%M.%Y %H:%M:%S", time.localtime(x))
 
+        link_list = ''
+        print(links)
+
+        for i in links:
+            link_list += f'<p>{i}</p>'
+
         text = f'''<b>ВАЖНОЕ - Вы отметили данное сообщение</b>
                                 <p><b>Чат:</b> <i>{chat}</i></p>
                                 <p><b>Отправитель:</b> <i>{sender}</i></p>
@@ -217,7 +220,7 @@ def send_email(message):
                                 <br>
                                 <b>Вложения:</b>
                                 <br>
-                                <i>{links}</i>
+                                <i>{link_list}</i>
                                 '''
 
         email = get_user_email(message.from_user.id)[0]
@@ -226,14 +229,11 @@ def send_email(message):
 
         links = []
 
-        text = bot.reply_to(message, "Письмо отправлено на почту ✅")
-        bot.register_next_step_handler(text, listener)
+        bot.send_message(message.from_user.id, "Письмо отправлено на почту ✅")
 
     except Exception:
-        text = bot.send_message(message.chat.id, "Произошла ошибка при отправке письма")
-        bot.register_next_step_handler(text, listener)
+        bot.send_message(message.from_user.id, "Произошла ошибка при отправке письма")
 
 
 if __name__ == '__main__':
     bot.polling(none_stop=True, interval=0)
-
